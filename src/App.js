52b13d47134ebbc9489d3c0572e231705f51a84e
Chris Westberg <chriswestberg@Chriss-MBP.hsd1.ut.comcast.net>
2020-05-30 15:19:59 -0600

    MVP with dummyData

--- src/App.js
+++ src/App.js
@@ -2,32 +2,28 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import MovieCard from './components/MovieCard'
 import axios from 'axios'
+import dummyData from './dummyData'
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
   useEffect(() => {
-      axios
-        .get('https://reqres.in/api/users?page=2')
-        .then( res => {
-          console.log(res.data.data)
-          setData(res.data.data)
-        })
-        .catch(err => {
-          console.log(err)
-        })
-      }, [] )
-      console.log(data)
-  return data.map(e => {
-  return <MovieCard firstName={e.first_name} lastName={e.last_name} image={e.avatar}/>
-  })
-  //   <div className="App">
-  //     {/* <p>
-  //       Read through the instructions in the README.md file to build your NASA
-  //       app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-  //     </p> */}
-  //     <MovieCard />
-  //   </div>
-  // )
+    setData(dummyData)
+  }, [])
+  // useEffect(() => {
+  //     axios
+  //       .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  //       .then(res => {
+  //         console.log(res)
+  //       })
+  //       .catch(err => {
+  //         console.log(err)
+  //       })
+  //     }, [])
+  return (
+  <div>
+    <MovieCard  image={data.hdurl} title={data.title} explain={data.explanation}/>
+  </div>
+  );
 }
 
 export default App;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,39 +3,17 @@ import axios from 'axios'
 import styled from 'styled-components'
 
 const MovieCard = (props) => {
-    const Container = styled.div `
-        display: flex;
-        flex-direction: row;
-    `
-    const Name = styled.h1 ` 
-        width: 350px
-    `
-    const Box = styled.div `
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-
-    `
-    const Picture = styled.img`
-    // transform: scale(2);
-    border-radius: 10px;
-    border: 2px solid red;
-    cursor: pointer;
-    &:hover{ transform:scale(1.01)}
-`
-
-
-    return (
-        <Container>
-            <Box className='box'>
-                <Name>{props.firstName} {props.lastName}</Name>
-                <Picture src={props.image} alt={props.name}/>
-            </Box>
-         </Container>
-    
-        
-
+//    const 
+   console.log(props)
+    return( 
+    <div>
+        <div className='header'>
+            <h1>{props.title}</h1>
+            <h2>Celestial Wonders of the Universe</h2>
+        </div>
+        <img src={props.image} />
+        <p>{props.explain}</p>
+    </div>
     );
 }
 
