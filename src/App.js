64ba8f1e5a756ca9a2a95f6ce7cdf4e8277f80f8
Chris Westberg <chriswestberg@Chriss-MBP.hsd1.ut.comcast.net>
2020-05-30 15:22:42 -0600

    Met MVP with axios request

--- src/App.js
+++ src/App.js
@@ -6,19 +6,20 @@ import dummyData from './dummyData'
 
 function App() {
   const [data, setData] = useState({});
-  useEffect(() => {
-    setData(dummyData)
-  }, [])
   // useEffect(() => {
-  //     axios
-  //       .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-  //       .then(res => {
-  //         console.log(res)
-  //       })
-  //       .catch(err => {
-  //         console.log(err)
-  //       })
-  //     }, [])
+  //   setData(dummyData)
+  // }, [])
+  useEffect(() => {
+      axios
+        .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+        .then(res => {
+          console.log(res)
+          setData(res.data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      }, [])
   return (
   <div>
     <MovieCard  image={data.hdurl} title={data.title} explain={data.explanation}/>
